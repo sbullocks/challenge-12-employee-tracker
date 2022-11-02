1c746674593b17c9e1f01141c8763d86d887d977
sbullocks <sbullocks@gmail.com>
2022-11-02 15:38:09 -0400

    added INSERT for roles.

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,14 @@ VALUES ("Engineering"),
        ("Finance),
        ("Legal"),
        ("Sales");
+
+INSERT INTO roles (department_id, title, salary)
+VALUES (1, "Sales Lead", "100000"),
+       (2, "Salesperson", "80000"),
+       (3, "Lead Engineer", "150000"),
+       (4, "Software Engineer", "120000"),
+       (5, "Account Manager", "160000"),
+       (6, "Accountant", "125000"),
+       (7, "Legal Team Lead", "250000"),
+       (8, "Lawler", "190000");
+
