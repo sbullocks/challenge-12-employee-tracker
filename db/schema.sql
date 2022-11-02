01bfa255c56400ba918862183992310d6310f6de
sbullocks <sbullocks@gmail.com>
2022-11-02 16:16:42 -0400

    Imported and requried MySQL2.

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,12 @@ USE employer_db;
 
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(255)
 );
 
 CREATE TABLE roles (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
+  title VARCHAR(255) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
   FOREIGN KEY (department_id) REFERENCES department(id)  
@@ -18,8 +18,8 @@ CREATE TABLE roles (
 
 CREATE TABLE employee (
     id INT auto_increment primary KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
     FOREIGN KEY (manager_id) REFERENCES roles(id) 
---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
