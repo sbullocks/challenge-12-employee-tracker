6043926bc303b26c9de1f4e6a48e11fa7e7dfe97
sbullocks <sbullocks@gmail.com>
2022-11-02 17:25:26 -0400

    removed functions to write file and create markdown. not needed for this.

--- index.js
+++ index.js
@@ -18,38 +18,45 @@ const db = mysql.createConnection(
 // Created an array of questions for user input
 const questions = [
   {
-      type: 'input',
+      type: 'list',
       message: 'View All Departments',
+      choices: ["ISC", "MIT", "Mozilla Public License 2.0"],
       name: 'viewAll',
   },
   {
-      type: 'input',
+      type: 'list',
       message: 'View All Roles',
+      choices: ["ISC", "MIT", "Mozilla Public License 2.0"],
       name: 'allRoles',
   },
   { 
-      type: 'input',
+      type: 'list',
       message: 'View All Employees',
+      choices: ["ISC", "MIT", "Mozilla Public License 2.0"],
       name: 'allEmployees',
   },
   { 
-      type: 'input',
+      type: 'list',
       message: 'Add A Department',
+      choices: ["ISC", "MIT", "Mozilla Public License 2.0"],
       name: 'addDepartment',
   },
   { 
-      type: 'input',
+      type: 'list',
       message: 'Add A Role',
+      choices: ["ISC", "MIT", "Mozilla Public License 2.0"],
       name: 'addRole',
   },
   { 
-      type: 'input',
+      type: 'list',
       message: 'Add An Employee',
+      choices: ["ISC", "MIT", "Mozilla Public License 2.0"],
       name: 'addEmployee',
   },
   { 
-      type: 'input',
+      type: 'list',
       message: 'Update An Employee Role',
+      choices: ["ISC", "MIT", "Mozilla Public License 2.0"],
       name: 'updateEmployeeRole',
   },
   // { 
@@ -70,23 +77,24 @@ const questions = [
   // },
 ];
 
-// Created a function to write README file
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, function (err) {
-      if (err) {
-          console.log(err);
-      } else {
-          console.log('README file successfully created!');
-      }
-  })
-};
+// Created a function to write README file... not creating any files.
+// function writeToFile(fileName, data) {
+//   fs.writeFile(fileName, data, function (err) {
+//       if (err) {
+//           console.log(err);
+//       } else {
+//           console.log('README file successfully created!');
+//       }
+//   })
+// };
 
-// Created a function to initialize app
-function init() {
-  inquirer.prompt(questions).then(function (data) {
-    writeToFile("README.md", generateMarkdown(data));
-  });
-  };
+// Created a function to initialize app... not needed for this, not writing a file.
+// function init() {
+//   inquirer.prompt(questions).then(function (data) {
+//     writeToFile("README.md", generateMarkdown(data));
+//   });
+//   };
+
   
   // Function call to initialize app
   init();
