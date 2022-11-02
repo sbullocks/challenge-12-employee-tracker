e54414a24d90253965592ffcce1a024c8857b8fc
sbullocks <sbullocks@gmail.com>
2022-11-02 16:11:40 -0400

    fixed errors in schema file so it runs in dbeaver.

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,24 @@ CREATE DATABASE employer_db;
 USE employer_db;  
 
 CREATE TABLE department (
-  id: INT NOT NULL AUTO_INCREMENT,
-  name: VARCHAR(30),
-  PRIMARY KEY (id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
-  title: VARCHAR(30) NOT NULL,
-  salary: DECIMAL NOT NULL,
-  department_id: INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department_id  
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)  
 );
 
 CREATE TABLE employee (
-    id: INT NOT NULL AUTO_INCREMENT,
-    first_name: VARCHAR(30) NOT NULL,
-    last_name: VARCHAR(30) NOT NULL,
-    role_id: INT NOT NULL,
-    manager_id: INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id) 
+    id INT auto_increment primary KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (manager_id) REFERENCES roles(id) 
 );
 
