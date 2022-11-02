fdcb2ab531d0e57886fbf2ecde6c368dd22b1611
sbullocks <sbullocks@gmail.com>
2022-11-02 16:39:25 -0400

    removed middleware and ports.

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const mysql = require('mysql2');
 // Import and rquire asciiart-logo
 const logo = require('asciiart-logo');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
 // Connect to database
 const db = mysql.createConnection(
     {
