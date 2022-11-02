a184bcaea5a905ecf3033b945d1637b830891fa9
sbullocks <sbullocks@gmail.com>
2022-11-02 15:31:53 -0400

    created table for department.

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS employer_db;
 CREATE DATABASE employer_db;
 
 USE employer_db;  
+
+CREATE TABLE department (
+  id: INT NOT NULL AUTO_INCREMENT,
+  name: VARCHAR(30),
+  PRIMARY KEY (id)
+);
